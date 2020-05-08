18cc1447ff5580483cd9efbe5ff6cceb9e04dd04
Zeeshan <zeeshan@plerosys.com>
2020-05-09 02:48:53 +0800

    update mailchimp image only if viewable present

--- app/models/spree_mailchimp_ecommerce/spree/image_decorator.rb
+++ app/models/spree_mailchimp_ecommerce/spree/image_decorator.rb
@@ -2,22 +2,22 @@ module SpreeMailchimpEcommerce
   module Spree
     module ImageDecorator
       def self.prepended(base)
-        base.after_create :update_mailchimp_product
-        base.after_update :update_mailchimp_product
-        base.after_destroy :update_mailchimp_product
+        base.after_create :update_mailchimp_product, if: Proc.new {|img| img.viewable_id.present? }
+        base.after_update :update_mailchimp_product, if: Proc.new {|img| img.viewable_id.present? }
+        base.after_destroy :update_mailchimp_product, if: Proc.new {|img| img.viewable_id.present? }
       end
 
       private
 
       def update_mailchimp_product
-
+        return true unless viewable_id.present? # no need to proceed
         find_product.mailchimp_product.each do |pro|
           ::SpreeMailchimpEcommerce::UpdateProductJob.perform_later(pro)
-        end        
+        end
       end
 
       def find_product
-        ::Spree::Variant.unscoped.find(viewable_id).product
+        ::Spree::Variant.unscoped.find_by_id(viewable_id)&.product
       end
     end
   end
