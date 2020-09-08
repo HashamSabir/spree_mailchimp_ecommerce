34b4b7b6db571263c9a9de11df8e83e45ab6b00f
Zeeshan <zeeshan@plerosys.com>
2020-09-08 23:00:29 +0800

    copy mailchimp for different stores

--- app/models/mailchimp_setting.rb
+++ app/models/mailchimp_setting.rb
@@ -2,8 +2,18 @@ class MailchimpSetting < ActiveRecord::Base
   validates :mailchimp_api_key, :mailchimp_store_id, :mailchimp_list_id, :mailchimp_store_name, :cart_url, presence: true
   validate :validate_only_one_store, on: :create
 
+  belongs_to :store, class_name: "Spree::Store"
+
   def validate_only_one_store
-    errors.add(:base, "only one store allowed") unless MailchimpSetting.count.zero?
+    # errors.add(:base, "only one store allowed") unless MailchimpSetting.count.zero?
+    errors.add(:base, "only one store allowed") if MailchimpSetting.where(store_id: self.store_id).present?
+  end
+
+  def already_exist?
+    attrs = self.attributes.slice("mailchimp_api_key", "mailchimp_list_id", "mailchimp_store_name")
+    other_settings = MailchimpSetting.where(attrs)
+    other_settings = other_settings - [self]
+    other_settings.any?
   end
 
   def create_store_id
--- lib/spree_mailchimp_ecommerce/version.rb
+++ lib/spree_mailchimp_ecommerce/version.rb
@@ -1,3 +1,3 @@
 module SpreeMailchimpEcommerce
-  VERSION = "1.0.5"
+  VERSION = "1.0.6"
 end
