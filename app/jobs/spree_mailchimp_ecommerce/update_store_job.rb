61a927c7558b6b800c2013ff3288a7806e18828e
Zeeshan <zeeshan@plerosys.com>
2020-11-19 18:04:07 +0800

    update mailchimp store's currency

--- app/jobs/spree_mailchimp_ecommerce/update_store_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/update_store_job.rb
@@ -3,7 +3,8 @@ module SpreeMailchimpEcommerce
     def perform(mailchimp_setting)
       gibbon_store(mailchimp_setting.store_id).update(body: {
                             name: mailchimp_setting.mailchimp_store_name,
-                            email_address: mailchimp_setting.mailchimp_store_email
+                            email_address: mailchimp_setting.mailchimp_store_email,
+                            currency_code: (mailchimp_setting.store&.default_currency || ::Spree::Config[:currency])
       })
     end
   end
--- lib/spree_mailchimp_ecommerce/version.rb
+++ lib/spree_mailchimp_ecommerce/version.rb
@@ -1,3 +1,3 @@
 module SpreeMailchimpEcommerce
-  VERSION = "1.1.2"
+  VERSION = "1.1.3"
 end
