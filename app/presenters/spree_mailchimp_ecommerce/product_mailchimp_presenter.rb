b0f37656b631bbd80ad80d22571a770fda5a2196
userzee <zeeshan@plerosys.com>
2022-03-15 01:08:19 +0500

    sync product master variant

--- app/presenters/spree_mailchimp_ecommerce/product_mailchimp_presenter.rb
+++ app/presenters/spree_mailchimp_ecommerce/product_mailchimp_presenter.rb
@@ -17,7 +17,7 @@ module SpreeMailchimpEcommerce
           url: ("#{store.domain_url}/#{product.slug}" || ""),
           vendor: product.category&.name || "",
           image_url: image_url,
-          variants: variants.select{ |variant| variant["store_id"].eql? store.id.to_s },
+          variants: (variants + master_variant).select{ |variant| variant["store_id"].eql? store.id.to_s },
           store_id: store.id.to_s
         }.as_json
       end
@@ -26,7 +26,11 @@ module SpreeMailchimpEcommerce
     private
 
     def variants
-      product.has_variants? ? product.variants.map(&:mailchimp_variant).flatten.uniq : product.master.mailchimp_variant.uniq
+      product.variants.map(&:mailchimp_variant).flatten
+    end
+
+    def master_variant
+      product.master.mailchimp_variant
     end
 
     def image_url
